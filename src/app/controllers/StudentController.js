9c5e62fcdcc02aa8cb8959746fe707f872bd1c0d
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-27 15:51:25 -0300

    student controller - store

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,38 @@
+import * as Yup from 'yup';
 import Student from '../models/Student';
 
 class StudentControler {
-  async store() {}
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.integer().required(),
+      weight: Yup.integer().required(),
+      height: Yup.float().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    const studentExists = await Student.findOne({ where: { email } });
+
+    if (studentExists) {
+      return res
+        .status(400)
+        .json({ error: 'A Student with that email already exists' });
+    }
+
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+
+  async update(req, res) {}
 }
 
 export default StudentControler;
