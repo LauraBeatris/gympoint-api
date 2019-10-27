3ddf6917d9cf12a0516b8a142c4a6c7906ebebc3
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-27 14:30:17 -0300

    session controller - authorization

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,47 @@
+import * as Yup from 'yup';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, password } = req.body;
+
+    // Verifying if the user exists
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      // Then, not allowed to proceed
+      return res.status(401).json({ error: 'User not found. Unauthorized' });
+    }
+
+    // Password verification
+    if (!(await user.checkPassword(password))) {
+      // Then, not allowed to proceed
+      return res
+        .status(401)
+        .json({ error: "Password doesn't match. Unauthorized" });
+    }
+
+    const { id, name } = user;
+
+    // Returning user informations and the token with the id as the payload
+    return res.json({
+      id,
+      name,
+      email,
+      token: jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import SessionController from './app/controllers/SessionController';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
 routes.put('/users', UserController.update);
 
 export default routes;
