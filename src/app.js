45dce03ffd5de53f268f4d8fd8b48dbbbc75c560
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-27 12:21:26 -0300

    created app file and server prepared to start

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.testing' : '.env',
+});
+
+class App {
+  constructor() {
+    this.server = express();
+    this.isDev = process.env.NODE_ENV !== 'production';
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // Ready to receive request bodies in JSON format
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
