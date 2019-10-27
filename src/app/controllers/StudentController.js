5816aa15a17d58178380dad11314302e420a1250
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-27 16:09:13 -0300

    fixing student controller - store

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -8,28 +8,31 @@ class StudentControler {
       email: Yup.string()
         .email()
         .required(),
-      age: Yup.integer().required(),
-      weight: Yup.integer().required(),
-      height: Yup.float().required(),
+      age: Yup.number()
+        .integer()
+        .required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
     });
 
+    // Validating the input data
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
     const { email } = req.body;
 
+    // Verifying if there's another student register with the same email
     const studentExists = await Student.findOne({ where: { email } });
-
     if (studentExists) {
       return res
         .status(400)
-        .json({ error: 'A Student with that email already exists' });
+        .json({ error: 'A student with that email already exists' });
     }
 
-    const student = await Student.create(req.body);
+    const { id, name, height, weight } = await Student.create(req.body);
 
-    return res.json(student);
+    return res.json({ id, name, email, height, weight });
   }
 
   async update(req, res) {}
