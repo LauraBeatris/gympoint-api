fdbad4cdfd4caf0e8aa8b801a5a67be21c98788c
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-10 20:54:58 -0300

    updating and deleting registrations

--- src/app/controllers/RegistrationController.js
+++ src/app/controllers/RegistrationController.js
@@ -1,6 +1,7 @@
 import Joi from 'joi';
 import { parseISO, addMonths } from 'date-fns';
 import Registration from '../models/Registration';
+import Student from '../models/Student';
 import Plan from '../models/Plan';
 
 import validationSchema from '../../validationSchemas/registrations';
@@ -39,12 +40,86 @@ class RegistrationController {
       student_id,
     });
 
-    return res.json({ id, start_date, end_date, plan, student_id });
+    return res.json({ id, start_date, end_date, price, plan, student_id });
   }
 
-  async update(req, res) {}
+  async update(req, res) {
+    Joi.validate(req.body, validationSchema.update, err => {
+      if (err) return res.status(400).json({ err: err.details });
 
-  async delete(req, res) {}
+      return true;
+    });
+
+    const { registration_id } = req.params;
+
+    // Validating param
+    if (!registration_id) {
+      return res.status(400).json({ err: 'Registration id not provided' });
+    }
+
+    // Finding the registration and updating
+    const registration = await Registration.findOne({
+      where: { id: registration_id },
+      include: [
+        {
+          model: Plan,
+          as: 'plan',
+          attributes: ['id', 'title', 'price', 'duration'],
+        },
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['id', 'name', 'email', 'age', 'height', 'weight'],
+        },
+      ],
+    });
+
+    if (!registration) {
+      return res.status(404).json({ err: 'Registration not found' });
+    }
+
+    const {
+      id,
+      start_date,
+      end_date,
+      price,
+      plan,
+      student,
+    } = await registration.update(req.body);
+    await registration.save();
+
+    return res.json({
+      id,
+      start_date,
+      end_date,
+      price,
+      plan,
+      student,
+    });
+  }
+
+  async delete(req, res) {
+    const { registration_id } = req.params;
+
+    // Validating param
+    if (!registration_id) {
+      return res.status(400).json({ err: 'Registration id not provided' });
+    }
+
+    // Finding the registration
+    const registration = await Registration.findByPk(registration_id);
+
+    if (!registration) {
+      return res.status(404).json({ error: 'Registration not found' });
+    }
+
+    // Deleting the plan
+    await registration.destroy();
+
+    return res.json({
+      msg: `Registration with id ${registration.id} was successfully deleted`,
+    });
+  }
 
   async index(req, res) {}
 }
