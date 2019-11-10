49d9c49768b886f22eb70b4504490c2ec4ffa88e
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-10 16:07:06 -0300

    creating a plan

--- src/app/controllers/PlansController.js
+++ src/app/controllers/PlansController.js
@@ -1,5 +1,30 @@
+import Joi from 'joi';
+import Plan from '../models/Plan';
+import validationSchema from '../../validationSchemas/plans';
+
 class PlansController {
-  async store(req, res) {}
+  async store(req, res) {
+    Joi.validate(req.body, validationSchema.store, err => {
+      if (err) return res.status(400).json({ err: err.details });
+
+      return true;
+    });
+
+    const { title, duration, price } = req.body;
+
+    // Veryfing if there's an existing plan with the same name
+    const existingPlan = await Plan.findOne({ where: { title } });
+
+    if (existingPlan) {
+      return res
+        .status(400)
+        .json({ err: "There's already a plan with that name" });
+    }
+
+    const { id } = await Plan.create({ title, duration, price });
+
+    return res.json({ id, title, price, duration });
+  }
 
   async update(req, res) {}
 
