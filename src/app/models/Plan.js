f9926d495872ce6dc75c0ebb4dbafabeb811279e
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-10 15:40:31 -0300

    plans migration and model

---
+++ src/app/models/Plan.js
@@ -0,0 +1,18 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Plan extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        duration: Sequelize.INTEGER,
+        price: Sequelize.FLOAT,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Plan;
---
+++ src/database/migrations/20191110183519-create-plans.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('plans', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('plans');
+  },
+};
