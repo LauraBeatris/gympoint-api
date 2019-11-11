3994edcc47b82b77193156a59571a20a7cdefbb4
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-10 22:28:14 -0300

    listing and updating registrations

--- src/app/controllers/RegistrationController.js
+++ src/app/controllers/RegistrationController.js
@@ -57,34 +57,28 @@ class RegistrationController {
       return res.status(400).json({ err: 'Registration id not provided' });
     }
 
-    // Finding the registration and updating
-    const registration = await Registration.findOne({
-      where: { id: registration_id },
-      include: [
-        {
-          model: Plan,
-          as: 'plan',
-          attributes: ['id', 'title', 'price', 'duration'],
-        },
-        {
-          model: Student,
-          as: 'student',
-          attributes: ['id', 'name', 'email', 'age', 'height', 'weight'],
-        },
-      ],
-    });
+    // Finding the registration
+    const registration = await Registration.findByPk(registration_id);
 
     if (!registration) {
       return res.status(404).json({ err: 'Registration not found' });
     }
 
+    // If the plan was changed, it needs to change the price and date informations
+    if (!!req.body.plan_id && req.body.plan_id !== registration.plan_id) {
+      const { duration, price } = await Plan.findByPk(req.body.plan_id);
+      req.body.price = duration * price;
+      req.body.end_date = addMonths(registration.start_date, duration);
+    }
+
+    // Updating and saving
     const {
       id,
+      plan_id,
       start_date,
       end_date,
       price,
-      plan,
-      student,
+      student_id,
     } = await registration.update(req.body);
     await registration.save();
 
@@ -93,8 +87,8 @@ class RegistrationController {
       start_date,
       end_date,
       price,
-      plan,
-      student,
+      plan_id,
+      student_id,
     });
   }
 
@@ -121,7 +115,38 @@ class RegistrationController {
     });
   }
 
-  async index(req, res) {}
+  async index(req, res) {
+    const { page = 1, start_date, end_date, price, plan_id } = req.query;
+
+    const query = {};
+
+    // Conditionally building the query object
+    if (start_date) query.start_date = start_date;
+    if (end_date) query.end_date = end_date;
+    if (price) query.price = price;
+    if (plan_id) query.plan_id = plan_id;
+
+    const registrations = await Registration.findAll({
+      where: { ...query },
+      offset: (page - 1) * 10,
+      limit: 10,
+      attributes: ['id', 'start_date', 'end_date', 'price'],
+      include: [
+        {
+          model: Plan,
+          as: 'plan',
+          attributes: ['id', 'title', 'price', 'duration'],
+        },
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['id', 'name', 'email', 'age', 'height', 'weight'],
+        },
+      ],
+    });
+
+    return res.json(registrations);
+  }
 }
 
 export default new RegistrationController();
