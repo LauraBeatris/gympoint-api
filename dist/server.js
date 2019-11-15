85d9d523d2e1228968f3afb81eda1fd1d28b8acb
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-15 22:45:50 +0000

    deploy adjustments - added dist to .gitignore

--- dist/server.js
+++
@@ -1,3 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-
-_app2.default.listen(3333);
