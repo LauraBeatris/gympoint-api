6ae6c8f683ba07bb0fd4b4fc658abd7340457f95
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-23 18:35:17 -0300

    configuring jest with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "jest"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
@@ -13,12 +13,20 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
+  env: {
+    jest: true // now **/*.test.js files' env has both es6 *and* jest
+  },
   rules: {
     'import/no-cycle': 'error',
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     camelcase: "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }]
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   }
 };
