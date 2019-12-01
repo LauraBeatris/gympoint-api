0ab2ba08a957d116156c03a256c5a625a5787078
LauraBeatris <laurabeatriserafim@gmail.com>
2019-12-01 18:44:48 -0300

    applied services pattern

---
+++ src/app/services/CreateCheckinService.js
@@ -0,0 +1,54 @@
+import { differenceInCalendarDays, format, getDay } from 'date-fns';
+import Checkin from '../models/Checkin';
+import Student from '../models/Student';
+
+class CreateCheckinService {
+  async run({ student_id }) {
+    // Validating student id
+    if (!student_id) {
+      throw new Error('Student id not provided');
+    }
+
+    // Verifying if the student exists
+    const existingStudent = await Student.findByPk(student_id);
+
+    if (!existingStudent) {
+      throw new Error('Student not found');
+    }
+
+    const existingCheckins = await Checkin.findAll({ where: { student_id } });
+
+    // Verifying if the user made more than 1 checkin per day
+    existingCheckins.forEach(checkin => {
+      if (getDay(checkin.createdAt) === getDay(Date.now())) {
+        throw new Error(
+          `You have already done an checkin at ${format(
+            checkin.createdAt,
+            'dd/mm/yyyy'
+          )}`
+        );
+      }
+    });
+
+    // Verifying the last days and counting until now to calculate the limit of days (7)
+    const limit =
+      existingCheckins &&
+      existingCheckins.forEach(async checkin => {
+        if (differenceInCalendarDays(Date.now(), checkin.createdAt) > 7) {
+          // Deleting old checkins
+          return Checkin.destroy({ where: { student_id } });
+        }
+      });
+
+    // If exists more than 5, the students areb't allowed to do more checkins
+    if (existingCheckins.length >= 5) {
+      throw new Error('Limit of checkins was reached');
+    }
+
+    const checkin = await Checkin.create({ student_id });
+
+    return checkin;
+  }
+}
+
+export default new CreateCheckinService();
---
+++ src/app/services/CreateRegistrationService.js
@@ -0,0 +1,65 @@
+import { parseISO, addMonths, format } from 'date-fns';
+
+import Registration from '../models/Registration';
+import Student from '../models/Student';
+import Plan from '../models/Plan';
+import Queue from '../../lib/Queue';
+import RegistrationMail from '../jobs/RegistrationMail';
+
+class CreateRegistrationService {
+  async run({ plan_id, student_id, start_date }) {
+    console.log(plan_id);
+    // Finding for the choosed plan
+    const plan = await Plan.findByPk(plan_id);
+
+    if (!plan) {
+      throw new Error('Plan not found');
+    }
+
+    const { price: planPrice, duration } = plan;
+
+    // Generating the end date and the price based on the choosed plan
+    start_date = parseISO(start_date);
+    const end_date = addMonths(start_date, duration);
+    const price = planPrice * duration;
+
+    // Verifying if the user already have a registration
+    const existingRegistration = await Registration.findOne({
+      where: { student_id },
+    });
+
+    if (existingRegistration) {
+      throw new Error(
+        'This user already have a registration. Update the current one or delete'
+      );
+    }
+
+    const student = await Student.findByPk(student_id);
+
+    if (!student) {
+      throw new Error(
+        'This user already have a registration. Update the current one or delete'
+      );
+    }
+
+    const { id } = await Registration.create({
+      start_date,
+      end_date,
+      price,
+      plan_id,
+      student_id,
+    });
+
+    await Queue.add(RegistrationMail.key, {
+      start_date: format(start_date, "'At day' dd 'of' MMMM',' H:mm 'hours'"),
+      end_date: format(end_date, "'At day' dd 'of' MMMM',' H:mm 'hours'"),
+      price,
+      plan,
+      student,
+    });
+
+    return { id, start_date, end_date, price, plan, student_id };
+  }
+}
+
+export default new CreateRegistrationService();
