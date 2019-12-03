c1fff4471c96be4e6dffc891174fd0fe7045b773
LauraBeatris <laurabeatriserafim@gmail.com>
2019-12-03 14:27:47 -0300

    removed cached dist - only build for production

--- dist/bootstrap.js
+++
@@ -1,5 +0,0 @@
-"use strict";const dotenv = require('dotenv');
-
-dotenv.config({
-  path: process.env.NODE_ENV === 'production' && '.env',
-});
--- dist/config/redis.js
+++
@@ -1,6 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});const redisConfig = {
-  host: '127.0.0.1',
-  port: 6379,
-};
-
-exports. default = redisConfig;
