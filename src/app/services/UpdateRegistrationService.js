91a8609f2e6d06dfe5454eff5c3ef6fe51a0b280
LauraBeatris <laurabeatriserafim@gmail.com>
2019-12-29 23:12:55 -0300

    preventing to edit a registration with a student that already has one active

--- src/app/services/UpdateRegistrationService.js
+++ src/app/services/UpdateRegistrationService.js
@@ -1,6 +1,7 @@
 import { addMonths } from 'date-fns';
 import Registration from '../models/Registration';
 import Plan from '../models/Plan';
+import Student from '../models/Student';
 
 class UpdateRegistrationService {
   async run({ registration_id, plan_id, data }) {
@@ -16,13 +17,47 @@ class UpdateRegistrationService {
 
     // If the plan was changed, it needs to change the price and date informations
     if (!!plan_id && plan_id !== registration.plan_id) {
-      const { duration, price } = await Plan.findByPk(plan_id);
+      // Verifyng if the plan exists
+      const plan = await Plan.findByPk(plan_id);
+
+      if (!plan) {
+        throw new Error('Plan not found');
+      }
+
+      const { duration, price } = plan;
+
       newPrice = duration * price;
       end_date = addMonths(registration.start_date, duration);
     }
 
+    const { student_id } = data;
+    /* Verifying if the student exists */
+    if (student_id) {
+      const student = await Student.findByPk(student_id);
+      if (!student) {
+        throw new Error('Student not found');
+      }
+
+      /* Verifying if the passed student is the same as the current one */
+
+      if (registration.student_id !== student_id) {
+        /* Finding if there's an existing registration with that student */
+        const registrationExists = await Registration.findOne({
+          where: { student_id },
+        });
+
+        if (registrationExists)
+          throw new Error(
+            JSON.stringify({
+              err: 'A registration with that student already exists',
+              contentMessage: 'Já existe uma matrícula com esse aluno.',
+            })
+          );
+      }
+    }
+
     // Updating and saving
-    const { id, start_date, student_id } = await registration.update(data);
+    const { id, start_date } = await registration.update(data);
     await registration.save();
 
     return {
